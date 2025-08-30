68b9f3d01cf132c32397b99793d20e8f4e7e8064
fgwsz <3191000458@qq.com>
2025-08-31 01:17:12 +0800

    fix CHECK/ASSERT宏参数展开多次求值的问题

--- include/neotest/condition_info.h
+++ include/neotest/condition_info.h
@@ -23,37 +23,44 @@ struct ConditionInfo{
         __FILE__ \
         ,NEOTEST_TO_STRING(__LINE__) \
         ,NEOTEST_TO_STRING(__VA_ARGS__) \
-        ,static_cast<bool>(__VA_ARGS__) \
+        ,static_cast<bool>(__VA_ARGS__) /*求值*/ \
     } \
 //
 #define NEOTEST_MAKE_CONDITION_INFO_BY_UNARY_OPERATOR(operator__,...) \
-    ::neotest::ConditionInfo{ \
-        __FILE__ \
-        ,NEOTEST_TO_STRING(__LINE__) \
-        ,( \
-            ::std::ostringstream{} \
-                <<::std::boolalpha \
-                <<NEOTEST_TO_STRING(operator__) " " \
-                <<"("<<(__VA_ARGS__)<<")" \
-        ).str() \
-        ,static_cast<bool>(operator__ (__VA_ARGS__)) \
-    } \
+    [&](void)->::neotest::ConditionInfo{ \
+        auto const& rhs_ref=(__VA_ARGS__); \
+        return ::neotest::ConditionInfo{ \
+            __FILE__ \
+            ,NEOTEST_TO_STRING(__LINE__) \
+            ,( \
+                ::std::ostringstream{} \
+                    <<::std::boolalpha \
+                    <<NEOTEST_TO_STRING(operator__) " " \
+                    <<"("<<rhs_ref<<")" \
+            ).str() \
+            ,static_cast<bool>(operator__ rhs_ref) \
+        }; \
+    }() \
 //
 #define NEOTEST_MAKE_CONDITION_INFO_BY_BINARY_OPERATOR( \
     operator__,lhs__,... \
 ) \
-    ::neotest::ConditionInfo{ \
-        __FILE__ \
-        ,NEOTEST_TO_STRING(__LINE__) \
-        ,( \
-            ::std::ostringstream{} \
-                <<::std::boolalpha \
-                <<"("<<(lhs__)<<")" \
-                <<" " NEOTEST_TO_STRING(operator__) " " \
-                <<"("<<(__VA_ARGS__)<<")" \
-        ).str() \
-        ,static_cast<bool>((lhs__) operator__ (__VA_ARGS__)) \
-    } \
+    [&](void)->::neotest::ConditionInfo{ \
+        auto const& lhs_ref=(lhs__); \
+        auto const& rhs_ref=(__VA_ARGS__); \
+        return ::neotest::ConditionInfo{ \
+            __FILE__ \
+            ,NEOTEST_TO_STRING(__LINE__) \
+            ,( \
+                ::std::ostringstream{} \
+                    <<::std::boolalpha \
+                    <<"("<<lhs_ref<<")" \
+                    <<" " NEOTEST_TO_STRING(operator__) " " \
+                    <<"("<<rhs_ref<<")" \
+            ).str() \
+            ,static_cast<bool>(lhs_ref operator__ rhs_ref) \
+        }; \
+    }() \
 //
 
 #endif//NEOTEST_CONDITION_INFO_H
